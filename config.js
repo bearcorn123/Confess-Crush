dfe1d82cd93e21c897749da5536adb5a8d01d466
bearcorn123 <95289709+bearcorn123@users.noreply.github.com>
2021-11-30 19:38:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "tặng cậu nè",
+    introTitle: 'Lphuong cute meo meo nhất hmt',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
